d211edde2d59b5434d517ebe7dd2b04a79bb3ebe
OscarMecerreyes <7836049@ALUMNOS.UFV.ES>
2024-06-03 18:15:28 +0200

    carpeta del back

---
+++ MeceExtraBack/src/main/java/MecerreyesExtraBack/MeceExtraBack/MeceExtraBackApplication.java
@@ -0,0 +1,13 @@
+package MecerreyesExtraBack.MeceExtraBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeceExtraBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeceExtraBackApplication.class, args);
+	}
+
+}
