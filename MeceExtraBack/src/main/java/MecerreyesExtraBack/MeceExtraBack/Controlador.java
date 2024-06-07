9d244141f7b071f2ea309bc29828157ce9b53257
OscarMecerreyes <7836049@ALUMNOS.UFV.ES>
2024-06-07 17:15:39 +0200

    visualiza los dos JSON

--- MeceExtraBack/src/main/java/MecerreyesExtraBack/MeceExtraBack/Controlador.java
+++ MeceExtraBack/src/main/java/MecerreyesExtraBack/MeceExtraBack/Controlador.java
@@ -1,9 +1,11 @@
 package MecerreyesExtraBack.MeceExtraBack;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -15,7 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 public class Controlador {
-
+    private List<cp_national_data> CPNationalList = null;
     private DatosMsCode datosMsCode = null;
     private ObjectMapper objectMapper = new ObjectMapper();
     private Set<String> allMSCODES = new HashSet<>();
@@ -56,5 +58,47 @@ public class Controlador {
         }
 
         return new ResponseEntity<>(datosMsCode, HttpStatus.OK);
+    }
+    @GetMapping("/Datos1/{mscode}")
+    public ResponseEntity<List<MsCode>> getDataByMsCode(@PathVariable String mscode) {
+        String filePath = "Ficheros/MsCode_json.json"; // Reemplaza con la ruta correcta
+
+        try {
+            // Lee el archivo JSON y lo deserializa en un objeto DatosMsCode si no se ha hecho ya
+            if (datosMsCode == null) {
+                datosMsCode = objectMapper.readValue(new File(filePath), DatosMsCode.class);
+            }
+
+            // Obtener la lista de objetos asociada al MsCode indicado
+            List<MsCode> msCodeList = datosMsCode.getData().get(mscode);
+
+            if (msCodeList == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(msCodeList, HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    // Endpoint para obtener datos de cp_national_data
+    @GetMapping("/Datos2")
+    public ResponseEntity<List<cp_national_data>> getDatosCPN() {
+        String filePath = "Ficheros/cp-national-datafile.json"; // Reemplaza con la ruta correcta
+
+        try {
+            // Lee el archivo JSON y lo deserializa en una lista de objetos cp_national_data
+            if (CPNationalList == null) {
+                CPNationalList = objectMapper.readValue(new File(filePath), new TypeReference<List<cp_national_data>>() {});
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(CPNationalList, HttpStatus.OK);
     }
 }
--- MeceExtraBack/src/main/java/MecerreyesExtraBack/MeceExtraBack/cp_national_data.java
+++ MeceExtraBack/src/main/java/MecerreyesExtraBack/MeceExtraBack/cp_national_data.java
@@ -1,5 +1,133 @@
 package MecerreyesExtraBack.MeceExtraBack;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+
 public class cp_national_data {
 
-}
+        @JsonProperty("_id")
+        private String _id;
+        @JsonProperty("mscode")
+        private String mscode;
+        @JsonProperty("year")
+        private String year;
+        @JsonProperty("estCode")
+        private String estCode;
+        @JsonProperty("estimate")
+        private float estimate;
+        @JsonProperty("se")
+        private float se;
+        @JsonProperty("lowerCIB")
+        private float lowerCIB;
+
+
+        @JsonProperty("upperCIB")
+        private float upperCIB;
+        @JsonProperty("flag")
+        private String flag;
+
+        public cp_national_data() {
+        }
+
+        public cp_national_data(String _id, String mscode,String year,String estCode,float estimate,float se,
+                      float lowerCIB,float upperCIB,String flag) {
+            this._id = _id;
+            this.mscode = mscode;
+            this.year = year;
+            this.estCode = estCode;
+            this.estimate = estimate;
+            this.se = se;
+            this.lowerCIB = lowerCIB;
+            this.upperCIB = upperCIB;
+            this.flag = flag;
+        }
+
+        public String get_id() {
+            return _id;
+        }
+
+        public void set_id(String _id) {
+            this._id = _id;
+        }
+
+        public String getMscode() {
+            return mscode;
+        }
+
+        public void setMscode(String mscode) {
+            this.mscode = mscode;
+        }
+
+        public String getYear() {
+            return year;
+        }
+
+        public void setYear(String year) {
+            this.year = year;
+        }
+
+        public String getEstCode() {
+            return estCode;
+        }
+
+        public void setEstCode(String estCode) {
+            this.estCode = estCode;
+        }
+
+        public float getEstimate() {
+            return estimate;
+        }
+
+        public void setEstimate(float estimate) {
+            this.estimate = estimate;
+        }
+
+        public float getSe() {
+            return se;
+        }
+
+        public void setSe(float se) {
+            this.se = se;
+        }
+
+        public float getLowerCIB() {
+            return lowerCIB;
+        }
+
+        public void setLowerCIB(float lowerCIB) {
+            this.lowerCIB = lowerCIB;
+        }
+
+        public float getUpperCIB() {
+            return upperCIB;
+        }
+
+        public void setUpperCIB(float upperCIB) {
+            this.upperCIB = upperCIB;
+        }
+
+        public String getFlag() {
+            return flag;
+        }
+
+        public void setFlag(String flag) {
+            this.flag = flag;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "_id='" + _id + '\'' +
+                    ", mscode='" + mscode + '\'' +
+                    ", year='" + year + '\'' +
+                    ", estCode=" + estCode + '\'' +
+                    ", estimate='" + estimate + '\'' +
+                    ", se='" + se + '\'' +
+                    ", lowerCIB='" + lowerCIB + '\'' +
+                    ", upperCIB='" + upperCIB + '\'' +
+                    ", flag='" + flag + '\'' +
+                    '}';
+        }
+    }
+
